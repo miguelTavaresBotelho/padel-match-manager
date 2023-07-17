abb8f46f9d10ee8722dba128ee4a587298512508
Miguel Botelho <miguel.t.b@hotmail.com>
2023-07-17 18:21:58 +0100

    Your commit message

---
+++ src/main/java/com/padelmatchmanager/padelmatchmanager/PadelMatchManagerApplication.java
@@ -0,0 +1,13 @@
+package com.padelmatchmanager.padelmatchmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadelMatchManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PadelMatchManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/padelmatchmanager/padelmatchmanager/PadelMatchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padelmatchmanager.padelmatchmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelMatchManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
